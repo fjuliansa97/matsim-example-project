1d1bf2f9ea56a59387ebee6190b67f0fb5d664ed
Julian Sandoval <ge32zeg@tum.de>
2022-05-02 09:47:21 +0200

    02.05.2022

---
+++ src/main/java/org/matsim/week_2/NetworkExercise.java
@@ -0,0 +1,45 @@
+package org.matsim.week_2;
+
+import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.network.*;
+import org.matsim.core.network.NetworkUtils;
+import org.matsim.core.utils.geometry.CoordUtils;
+
+public class NetworkExercise {
+
+    public static void main(String[] args) {
+        Network emptyNetwork = NetworkUtils.createNetwork();
+
+        NetworkFactory networkFactory = emptyNetwork.getFactory();
+
+        Coord a = CoordUtils.createCoord(0,0);
+        Coord b = CoordUtils.createCoord(0,1000);
+
+        Node nodeA = networkFactory.createNode(Id.createNodeId("nodeA"), a);
+        Node nodeB = networkFactory.createNode(Id.createNodeId("nodeB"), b);
+
+        emptyNetwork.addNode(nodeA);
+        emptyNetwork.addNode(nodeB);
+
+        Link link1 = networkFactory.createLink(Id.createLinkId("link1"), nodeA, nodeB);
+        Link link2 = networkFactory.createLink(Id.createLinkId("link2"), nodeB, nodeA);
+
+        emptyNetwork.addLink(link1);
+        emptyNetwork.addLink(link2);
+
+        link1.getFreespeed(13.8);
+        link1.setCapacity(1800);
+        link1.getLength(1000);
+        link1.setNumberOfLanes(2);
+
+        link2.setFreespeed(13.8);
+        link2.getCapacity(1800);
+        link2.setLength(1000);
+        link2.getNumberOfLanes(2);
+
+        NetworkWriter writer = new NetworkWriter();
+        writer.write("scenarios/week2/testNetwork");
+    }
+
+}
---
+++ src/main/java/org/matsim/week_2/PopulationExercise.java
@@ -0,0 +1,50 @@
+package org.matsim.week_2;
+
+import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.population.*;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.population.PopulationUtils;
+
+public class PopulationExercise {
+
+    public static void main(String[] args) {
+
+//        Config config = ConfigUtils.createConfig();
+//
+//        Population emptyPopulation = PopulationUtils.createPopulation(config);
+//
+//        PopulationFactory popFactory = emptyPopulation.getFactory();
+//
+//        Person person1 = popFactory.createPerson(Id.createPersonId(1));
+//
+//        emptyPopulation.addPerson(person1);
+//
+//        Plan plan1 = popFactory.createPlan();
+//
+//
+//        Activity homeAct = popFactory.createActivityFromCoord("home", new Coord(0,0));
+//        homeAct.setEndTime(8*60*60);
+//
+//
+//        Leg leg2work = popFactory.createLeg(TransportMode.car);
+//        plan1.addLeg(leg2work);
+//
+//        Activity workAct = popFactory.createActivityFromCoord("home", new Coord(990,0));
+//        workAct.setEndTime(17*60*60);
+//        plan1.addActivity(workAct);
+//
+//        Leg leg2home = popFactory.createLeg(TransportMode.pt);
+//        plan1.addLeg(leg2home);
+//
+//        Activity endAct = popFactory.createActivityFromCoord("home", new Coord(0,0));
+//        plan1.addActivity(endAct);
+//
+//        PopulationWriter writer = new PopulationWriter();
+//        writer.write("scenarios/week2/testPopulation.xml");
+
+    }
+
+}
